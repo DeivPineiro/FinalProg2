97654213ec6ad29590a7d1d938c99193de310bda
David Ezequiel Piñeiro <101911710+DeivPineiro@users.noreply.github.com>
2024-09-05 12:11:23 -0300

    push Conexion

--- classes/Conexion.php
+++ classes/Conexion.php
@@ -2,10 +2,10 @@
 
 class Conexion{
 
-    private const DB_SERVER = "localhost";
-    private const DB_USER = "root";
-    private const DB_PASS = "";
-    private const DB_NAME = "fendardo_base";  
+    private const DB_SERVER = "sql311.byetcluster.com";
+    private const DB_USER = "if0_35994670";
+    private const DB_PASS = "Cortazar123";
+    private const DB_NAME = "if0_35994670_prog2";  
     private const DB_DSN = "mysql:host=".self::DB_SERVER.";dbname=".self::DB_NAME.";charset=utf8mb4";
 
     private static ?PDO $db = null;
@@ -16,22 +16,22 @@ class Conexion{
         try {
            self::$db = new PDO(self::DB_DSN,self::DB_USER,self::DB_PASS, array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
         } catch (Exception $e) {
-            die ("Error al conectar a la base de datos");
+            die ("Error al conectar a la base de datos" . "Error=>" . $e );
         }
 
     }
 
-public static function getConexion(): PDO
-{
+    public static function getConexion(): PDO
+    {
 
-    if(self::$db === null){
+        if(self::$db === null){
 
-        self::conectar();
+            self::conectar();
+
+        }
+
+        return self::$db;
 
     }
-
-    return self::$db;
-
-}
 
 }
